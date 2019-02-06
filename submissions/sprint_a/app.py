6ed39c0306ca1a96144355cf9b6039b88a5ccc87
Bob Hunt <bob.hunt.email@gmail.com>
2019-02-06 14:35:01 -0500

    Modified routes in app.py to return templates, then modified them back to return stubbed json.

--- submissions/sprint_a/app.py
+++ submissions/sprint_a/app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def generate_epithets():
+    # return render_template('insults.html')
     return jsonify({"epithets":[]})
 
 @app.route('/vocabulary')
 def vocabulary():
+    # return render_template('vocab.html')
     return jsonify({"vocabulary": {}})
 
 if __name__ == '__main__':
