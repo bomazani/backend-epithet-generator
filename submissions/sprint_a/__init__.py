d44ed064e5c0f1ca359566cb527cf41c1437bb71
Bob Hunt <bob.hunt.email@gmail.com>
2019-02-06 13:58:40 -0500

    Correctly loading the environmental variables automatically from the .env file. Routes are properly defined.

--- submissions/sprint_a/__init__.py
+++ submissions/sprint_a/__init__.py
@@ -6,14 +6,11 @@ def configure_app():
     from flask import Flask
     app = Flask(__name__)
 
-    PROJECT_ROOT = os.path.dirname(".")
-    # PROJECT_ROOT = os.path.abspath('.') ?????
+    PROJECT_ROOT = os.path.abspath('.')
+    env_path = os.path.join(PROJECT_ROOT, '.env')
+    dotenv.load_dotenv(env_path)
+
+    return
+
     
-    # Use dotenv.load_dotenv(), os.path.join(), and the PROJECT_ROOT variable to load environment variables automatically from the .env file.
-    dotenv.load_dotenv(os.path.abspath('.'))
-    # dotenv.load_dotenv(PROJECT_ROOT) ?????
-
-    # Instantiate and return an instance of Flask from the configure_app function.
-
-    # Use the configure_app function to assign an instance of the Flask application to a variable labeled app.
 app = configure_app()
